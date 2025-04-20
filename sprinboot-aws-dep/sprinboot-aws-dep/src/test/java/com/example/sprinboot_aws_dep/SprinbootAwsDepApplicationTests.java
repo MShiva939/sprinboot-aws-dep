31365a01a78d94128fda79cdf5e4427680ff43b3
MShiva939 <mdshiva125@gmail.com>
2025-04-20 14:35:36 +0530

    Initial commit

---
+++ sprinboot-aws-dep/sprinboot-aws-dep/src/test/java/com/example/sprinboot_aws_dep/SprinbootAwsDepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprinboot_aws_dep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbootAwsDepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
